aaacfd396135ffd5c1c6a812e779b99f472d8571
BarckCode <barckcode@gmail.com>
2021-10-19 13:14:31 +0200

    Completed Endpoint: /users

--- api/routes/users_routes.py
+++ api/routes/users_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Response
+from sqlalchemy.sql.expression import null
 from starlette.status import HTTP_204_NO_CONTENT
 
 # Internal Modules
@@ -22,3 +23,37 @@ async def get_user_by_id(id: str):
     return db_connection.execute(users_table.select().where(users_table.c.id == id)).first()
 
 
+@users_endpoint.post("/users", response_model=UsersModel, tags=["Users"])
+async def add_new_user(user: UsersModel):
+    new_user = {
+        "id": user.id,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "nickname": user.nickname
+    }
+
+    validation = db_connection.execute(users_table.select().where(users_table.c.id == user.id)).first()
+
+    if validation == None:
+        db_connection.execute(users_table.insert().values(new_user))
+        return db_connection.execute(users_table.select().where(users_table.c.id == user.id)).first()
+    else:
+        return db_connection.execute(users_table.select().where(users_table.c.id == user.id)).first()
+
+
+@users_endpoint.delete("/users/{id}", status_code=HTTP_204_NO_CONTENT, tags=["Users"])
+async def delete_user_by_id(id: str):
+    db_connection.execute(users_table.delete().where(users_table.c.id == id))
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@users_endpoint.put("/users/{id}", response_model=UsersModel, tags=["Users"])
+async def update_user_by_id(id: str, user: UsersModel):
+    db_connection.execute(users_table.update().values(
+        id = user.id,
+        first_name = user.first_name,
+        last_name = user.last_name,
+        nickname = user.nickname
+    ).where(users_table.c.id == id))
+
+    return db_connection.execute(users_table.select().where(users_table.c.id == user.id)).first()
